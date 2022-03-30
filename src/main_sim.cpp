a09004865d811f744bd0a008988bfa04ec133f43
stefanwerleman <stefanwerleman@knights.ucf.edu>
2022-03-29 21:25:06 -0400

    Fixed reading table bug

--- src/main_sim.cpp
+++ src/main_sim.cpp
@@ -44,10 +44,10 @@ void print_results(ArgumentWrapper arguments, Controller *ctrl)
     else if (arguments.predictor == "hybrid")
     {
         std::cout << "FINAL CHOOSER CONTENTS" << std::endl;
-        for (int entry = 0; entry < ctrl->b->size; entry++)
-        {
-            std::cout << entry << "\t" << ctrl->b->table[entry] << std::endl;
-        }
+        // for (int entry = 0; entry < ctrl->b->size; entry++)
+        // {
+        //     std::cout << entry << "\t" << ctrl->b->table[entry] << std::endl;
+        // }
     }
     else if (arguments.predictor == "smith")
     {
